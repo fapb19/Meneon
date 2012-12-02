57643a3d4c016863bda43cde9259d6d4e256c124
Javier Meza Schmidt <jmezaschmidt@gmail.com>
2012-12-01 21:36:12 -0600

    Cambios

---
+++ Albergues.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Alerta Desastres</title>
+<link href="CSS/Estilos.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="wrapper">
+  <div id="header">
+    <div id="navbar">
+    	<a href="default.php">HOME</a>
+        <a href="">MAPA</a>
+        <a href="">ALBERGUES</a>
+        <a href="">REDES SOCIALES</a>
+    </div>
+  </div>
+  <div id="Container">
+  	<div id=Contenido>
+        <div style="float:left; width:50%">
+        <p>
+            <label>Seleccione la zona: </label>
+            <select id="listZona">
+                <option>GAM</option>
+                <option>Turrialba</option>
+                <option>San Carlos</option>
+            </select>
+        </p>
+        <br />
+        <button id="buttonCargar">Cargar Informacion</button> 
+        </div>
+        <div style="float:right; width:50%">
+        
+        </div>
+    
+    
+        
+    </select>
+    
+    
+    
+    
+    </div>
+    <div id="footer"><h5 align="center">© 2012 TAC</h5></div>
+  </div>
+</div>
+</body>
+</html>
