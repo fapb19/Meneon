fce8e18a04bf0462e187051e2ddc1b2be1ead278
Filiberto Pereira Bonilla <fapb19>
2012-12-02 14:29:59 -0600

    seis

--- default.php
+++ default.php
@@ -8,17 +8,62 @@
 <body>
 <div id="wrapper">
   <div id="header">
+  	<div id="imagenLogo">
+    <img src="IMAGENES/Que-Salvada-logo.png" width="187" height="70" alt="Q Salvada!" /></div>
     <div id="navbar">
-    	<a href="default.php">HOME</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <a href="Mapas.php">MAPAS</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <a href="Clima.php">CLIMA</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <a href="">ALBERGUES</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <a href="">REDES SOCIALES</a>
+    	<a href="default.php">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="Mapas.php">Mapas</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="Clima.php">Centros de Atención</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="">Reportar</a>
     </div>
+    <div id="banner">
+   	  <img src="IMAGENES/Header-home.png" alt="Q Salvada!" width="961" height="280" /></div>
+  	</div>
   </div>
   <div id="Container">
-  	<div id=Contenido>lhjhjkhjklhjk;j;</div>
-    <div id="footer"><h5 align="center">© 2012 TAC</h5></div>
+  	<div id="Restringido">
+    	<div class="encabezado">
+        	Acceso Restringido
+         </div>
+         <div id="Monitoreo">
+         	<div id="imMonitoreo">
+            	<img src="IMAGENES/Monitoreo-Btn.png" width="213" height="123" alt="CNE" />
+            </div>
+            <div id="footerMonitoreo">
+                Centro de monitoreo(Solo personal autorizado)</div>
+            </div>
+     	 </div>
+    </div>
+    <div id="CNE">
+         <div class="encabezado">
+            Enlaces de Interes
+         </div>
+         <div class="imEnlace">
+            <div class="imEnlaces"><img src="IMAGENES/CNE-Btn.png" width="213" height="123" alt="CNE" /></div>
+            <div class="footerEnlaces">Comisión Nacional de Prevención de Riesgos y Atención de Emergencias</div>
+         </div>
+    </div>
+    <div id="OVR">
+         <div class="encabezado">
+             <span>l</span>
+         </div>
+         <div class="imEnlaces">
+             <img src="IMAGENES/OVSICORI-Btn.png" width="213" height="123" alt="CNE" /></div>
+         <div class="footerEnlaces">
+             Observatorio Vulcanológico y Sismológico de Costa Rica, UNA
+         </div>
+     </div>
+     <div id="IMN">
+         <div class="encabezado">
+             <span>l</span>
+         </div>
+         <div class="imEnlaces">
+             <img src="IMAGENES/IMN-Btn.png" width="213" height="123" alt="CNE" /></div>
+         <div class="footerEnlaces">
+             Instituto Meteorólogico Nacional</div>
+         </div>
+      </div>
+    <div id="footer"><h5 align="right">©Q' Salvada 2012</h5></div>
   </div>
 </div>
 </body>
---
+++ mapadesalvadas.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+<style type="text/css">
+  html { height: 100% }
+  body { height: 100%; margin: 0px; padding: 0px }
+  #map_canvas { height: 100% }
+</style>
+<?php
+	include("MySQL/conexionDB.php");
+	$sql = "SELECT * FROM  `Salvadas` ;";
+	$result= mysql_query($sql);
+	print_r($result[1]);
+	mysql_close($con);
+	echo "<script type='text/javascript'>\n";
+	 
+	echo "function localizar(){	\n";
+		echo "navigator.geolocation.getCurrentPosition(ubicacion,error);\n";
+	echo "}\n";
+	 
+    echo "function ubicacion(posicion){\n";
+		echo "var contenedor = document.getElementById('mapa'); \n";
+		echo "var latitud = posicion.coords.latitude;\n";
+		echo "var longitud = posicion.coords.longitude;\n";
+			
+		echo "var latlng = new google.maps.LatLng(latitud, longitud);\n";
+		echo "var myOptions = {\n";
+			echo "zoom: 8,\n";
+			echo "center: latlng,\n";
+			echo "mapTypeId: google.maps.MapTypeId.ROADMAP\n";
+		echo "};\n";
+		echo "var map = new google.maps.Map(document.getElementById('map_canvas'),myOptions);\n";
+				
+		echo "marcarSitioActual(latlng,map);";
+		if($_GET){
+			$Distancia=$_GET['Distancia'];
+			$Tiempo=$_GET['Tiempo'];
+		}else{
+			$Distancia=10;
+			$Tiempo="Today";
+		}
+		echo "var latMax=latitud+".($Distancia/111).";\n";
+		echo "var latMin=latitud-".($Distancia/111).";\n";
+		echo "var longMax=longitud+".($Distancia/111).";\n";
+		echo "var longMin=longitud-".($Distancia/111).";\n";
+		
+		$indice=0; 
+		/*foreach ($Result as $Salvada){
+			/*if($indice==0){
+				$indice=1;
+			}else{
+				if($Tiempo=="Today"){
+					$hoy = date("d/m/y");
+					if("11/22/12"==$Sismo[0]){
+						echo "if((".$Sismo[7].">latMin)&&(".$Sismo[7]."<latMax)&&(".$Sismo[8].">longMin)&&(".$Sismo[8]."<longMax)){\n";
+						echo "var latlng=new google.maps.LatLng(".$Sismo[7].",".$Sismo[8].");\n";
+						echo "marcarLugar('".$Sismo[0]."','".$Sismo[1]."','".$Sismo[2]."','".$Sismo[3]."','".$Sismo[4]."','".$Sismo[5]."','".$Sismo[6]."',latlng,map);\n";
+						echo "}\n";
+					}
+				}else{
+					echo "if((".$Sismo[7].">latMin)&&(".$Sismo[7]."<latMax)&&(".$Sismo[8].">longMin)&&(".$Sismo[8]."<longMax)){\n";
+					echo "var latlng=new google.maps.LatLng(".$Sismo[7].",".$Sismo[8].");\n";
+					echo "marcarLugar('".$Sismo[0]."','".$Sismo[1]."','".$Sismo[2]."','".$Sismo[3]."','".$Sismo[4]."','".$Sismo[5]."','".$Sismo[6]."',latlng,map);\n";
+					echo "}\n";
+				}
+			}
+		}
+	echo "}\n";*/
+	echo "</script>\n";
+?>
+	
+	<script type="text/javascript">
+	function marcarSitioActual(LATLONG,mapa){
+		var marker = new google.maps.Marker({ //opciones
+      		position: LATLONG,
+      		//Decimos que la posición es la de la variable 'coordenadas'
+      		map: mapa,
+      		//Nombre del mapa
+      		title:"Esta es mi ubicación actual"
+      		//Titulo (visible cuando colocamos el ratón sobre el punto)
+  		});
+	}
+	
+	function marcarLugar(Fecha,Hora,Magnitud,Profundidad,Localizacion,Causa,Origen,LATLONG,mapa){
+		var texto = "Fecha:"+Fecha+"\nHora:"+Hora+"\nMagnitud:"+Magnitud+"\nProfundidad:"+Profundidad+"\nCausa:"+Causa+"\nLugar:"+Origen;
+		var marker = new google.maps.Marker({ //opciones
+      		position: LATLONG,
+      		//Decimos que la posición es la de la variable 'coordenadas'
+      		map: mapa,
+      		//Nombre del mapa
+      		title:texto
+      		//Titulo (visible cuando colocamos el ratón sobre el punto)
+  		});
+	}
+ 
+    function error(error){
+		if(error.code == 0)
+        	 alert("Error Desconocido");
+        else if(error.code == 1)
+            alert("No fue posible contactarte");
+        else if(error.code == 2)
+            alert("No hay una ubicacion disponible");
+        else if(error.code == 3)
+            alert("Tiempo agotado");
+        else
+            alert("Error Desconocido");
+	}
+</script>
+ 
+<script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=true"> </script>
+
+</head>
+<body onload="localizar()">
+  <div id="map_canvas" style="width:100%; height:100%"></div>
+</body>
+</html>
